1cb119cde304e53af1f136f626e3ea7106f25cc6
hajung <bover80@naver.com>
2026-05-06 17:19:31 +0900

    Java 조건문 -if,else -if의 중첩, else if(다중분기) -switch, case

---
+++ src/constate/IfExample.java
@@ -0,0 +1,19 @@
+package constate;
+
+public class IfExample {
+
+    // 자바는 기본적ㅇ로 main 함수가 있어여 프로그램 실행이 가능하다.
+    public static void main(String[] args) {
+
+        int score = 85;
+
+        if (score >= 70) {
+            System.out.println("점수가 70점 이상입니다. 합격입니다.");
+        } else {
+            System.out.println("점수가 70점 미만입니다. 불합격입니다 ㅜㅜ");
+        }
+        System.out.println("시험 치르느라 고생 많으셨어요!");
+    }
+}
+
+        // 코드의 흐름은 꼭 하나로 만난다.
---
+++ src/constate/SwitchExample.java
@@ -0,0 +1,25 @@
+package constate;
+
+public class SwitchExample {
+
+    public static void main(String[] args) {
+
+        String gender = "z";
+
+        // if문처럼 논리형 조건식이 들어가는게 아니라
+        // 분기를 나눌 기준값이 괄호 안에 들어갑니다. (문자열, 정수만 가능)
+        switch (gender) {
+            case "m" : case "M" : case "남자" : case "ㅡ" :
+                System.out.println("남자입니다.");
+                break;  // 해당 case만 실행하고 switch문을 종료해라!
+
+            // java 14버전 이후 개선된 case문
+            case "F", "f", "ㄹ", "여" :
+                System.out.println("여자입니다.");
+                break;
+
+            default: // case를 설정하지 않은 값들은 모두 default로 빠짐.
+                System.out.println("잘못된 성별입니다.");
+        }
+    }
+}
