4576408efc0af8a25e822822118a970d9adc4245
hajung <bover80@naver.com>
2026-05-07 21:52:06 +0900

    JAVA 메서드 기초 -springLogApp 클래스 생성 -간단한 기능 구현 - 중복되는 기능과 로직을 메서드화 하기

---
+++ src/oop/intro/SprintLogApp.java
@@ -0,0 +1,53 @@
+package oop.intro;
+
+public class SprintLogApp {
+
+    public static void main(String[] args) {
+
+        LearningLog javaLog = new LearningLog();
+        javaLog.title = "java 시작";
+        javaLog.minutes = 40;
+        javaLog.publicLog = true;
+
+        LearningLog gitLog = new  LearningLog();
+        gitLog.title = "Git 복습";
+        gitLog.minutes = 30;
+        gitLog.publicLog = false;
+
+        LearningLog conditionLog = new LearningLog();
+        conditionLog.title = "조건문 학습";
+        conditionLog.minutes = 75;
+        conditionLog.publicLog = true;
+
+        LearningLog loopLog = new LearningLog();
+        loopLog.title = "반복문 학습";
+        loopLog.minutes = 120;
+        loopLog.publicLog = true;
+
+        LearningLog[] logs = {javaLog, gitLog, conditionLog, loopLog};
+
+        System.out.println("==== 객체로 관리하는 학습 기록 ====");
+        for (int i = 0; i < logs.length; i++) {
+            logs[i].printSummary();
+        }
+
+        System.out.println();
+        System.out.println("==== 복습 필요 기록 ====");
+        for (LearningLog log : logs) {
+            if(log.needsReview()) {
+                log.printSummary();
+            }
+        }
+
+        System.out.println();
+        System.out.println("==== 공개 기록 ====");
+        for (LearningLog log : logs) {
+            if(!log.publicLog) {
+                continue;
+            }
+            log.printSummary();
+        }
+
+    }
+
+}
