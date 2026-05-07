2c2876f905eb0b87bd6c1e299ce86d5b8f463224
hajung <bover80@naver.com>
2026-05-07 11:19:54 +0900

    Java 반복문 -while문, for문 -break, continue

---
+++ src/loop/BreakExample.java
@@ -0,0 +1,23 @@
+package loop;
+
+public class BreakExample {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++) {
+            if (i == 5) break;
+            // 블록 내의 코드가 한 줄인 경우에는 괄호 생략이 가능합니다. (두줄은 안돼요!!)
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n=======================================");
+
+        outer: for(int i = 0; i <=2; i++) {
+            for(int j = 0; j <= i; j++) {
+                if(i == j ) break outer;
+                System.out.println(i + "-" + j);
+            }
+        }
+
+    }
+}
---
+++ src/loop/ContinueExample.java
@@ -0,0 +1,26 @@
+package loop;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++) {
+
+            if (i == 5) continue;
+            // for문에서 continue는 증감식이 실행됨.
+
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n=======================================");
+
+        int n = 1;
+        while (n <= 10) {
+            if (n == 5) continue;
+            // while문에 continue는 조건식이 흐름이 이동.
+            // 반복문이 끝나지 않는 문제가 발생할 수 있음!
+            System.out.print(n + " ");
+            n++;
+        }
+        System.out.println("\n반복문 종료");
+    }
+}
