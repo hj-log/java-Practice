fc93ad1fc3cf2725bc75e39a141ba6d9262e0ec6
hajung <bover80@naver.com>
2026-05-08 10:13:56 +0900

    jav 객체지향(정보 은닉과 캡슐화) -정보 은닉과 캡슐화를 위한 setter 메서드 작성 - 값을 돌려주기 위한 getter메서드 작성 -3항 연산식 소개

--- src/oop/access_modifier/LearningLog.java
+++ src/oop/access_modifier/LearningLog.java
@@ -9,15 +9,17 @@ public class LearningLog {
     private boolean publicLog;
 
     LearningLog() {
+
         System.out.println("기본 생성자 호출!");
     }
 
     LearningLog(String title, int minutes) {
+
         this(title, minutes, true);
     }
 
     LearningLog(String title, int minutes, boolean publicLog) {
-        this.title = title;
+        this.title = normalizeTitle(title);
         this.minutes = minutes;
         this.publicLog = publicLog;
     }
@@ -25,6 +27,7 @@ public class LearningLog {
     // private 접근 제한을 지정하니까, 제대로 된 값도 수정이 불가능한 것을 확인했습니다.
     // 필드 값을 대신 받아서 할당하고, 값을 돌려줄 수 있는 메서드를 활용해서 값을 보호하겠습니다.
     // 이 때 사용하는 메서드의 이름을 getter, setter라고 합니다.
+    // set + 필드이름으로 짓는 것이 관례, 직관적으로 이름으로 짓어도 됩니다.
 
     public void setMinutes(int minutes) {
         if (minutes <= 0) {
@@ -34,13 +37,53 @@ public class LearningLog {
         this.minutes += minutes;
     }
 
+    public void setTitle(String newTitle) {
+        this.title = normalizeTitle(newTitle);
+    }
+
+    // 이 메서드는 굳이 외부에서 알 필요가 없고, 호출할 일도 없기 때문에 private으로 설정해서
+    // 이 클래스 안에서만 사용할 수 있도록 범위를 지정
+    private String normalizeTitle(String newTitle) {
+        if (newTitle == null || newTitle.isBlank()) {
+            return "제목 없음";
+        }
+
+        return newTitle;
+    }
+
+    public  void hideFromPublic() {
+        this.publicLog = false;
+    }
+
+    public  void openTobePublic() {
+        this.publicLog = true;
+    }
+
 
     void printSummary() {
-        System.out.println(title + " - " + minutes + "분");
+        // 3항 연산식: 조건식의 결과에 따라 변수에 대입할 값을 다르게 할 수 있는 문법
+        // 논리형 조건식 ? 좌항 : 우항
+        // 논리형 조건식이 true라면 좌항의 값이, false라면 우항의 값이 도출됩니다.
+        String visibility = publicLog ? "공개" : "비공개";
+        System.out.println(title + " - " + minutes + "분 - ");
     }
 
     boolean needsReview() {
+
         return minutes < 60;
     }
 
+    // 외부로 필드값을 돌려주는 getter 메서드
+    // get + 필드이름으로 지어주는 것이 관례, boolean 타입의 값을 돌려주는 getter는 is로 시작하도록 이름을 짓습니다.
+    public String getTitle() {
+        return title;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public boolean isPublicLog() {
+        return publicLog;
+    }
 }
--- src/oop/access_modifier/SprintLogApp.java
+++ src/oop/access_modifier/SprintLogApp.java
@@ -11,6 +11,14 @@ public class SprintLogApp {
 //        javaLog.minutes = -34567;
 //        javalog.pubilcLog = false; 모든 필드가 private 접근 제한자를 지정했기 때문에 값을 창조하거나 수정이 불가능해집니다.
 
+        javaLog.setMinutes(200);
+        javaLog.setTitle("java 객체지향의 캡슐화");
+        javaLog.hideFromPublic();
 
+        gitLog.setMinutes(45);
+        gitLog.openTobePublic();
+
+        javaLog.printSummary();
+        gitLog.printSummary();
     }
 }
